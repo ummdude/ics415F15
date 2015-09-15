44e5e614adc035b5c3bfbfa96fe5ab9936060076
ummdude <greenfishlover@hotmail.com>
2015-09-15 11:23:42 -1000

    w4c1

--- IC/w4c1/javasc.js
+++ IC/w4c1/javasc.js
@@ -24,7 +24,14 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
+    $("#element1").mouseenter(function(){
+        $("#element2").style.color = '#00FF00';
+    });
+});
+
+$(document).ready(function(){
     $("#element4").mouseleave(function(){
         $("#element5").show();
+        $("#element5").style.color = '#00FF00';
     });
 });
